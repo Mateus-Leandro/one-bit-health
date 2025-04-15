94664fd075ac27c29d0221559cd6acf0469d1f34
mateus.chagas <mateus.chagas@avancoinfo.com.br>
2025-04-15 11:04:32 -0300

    :sparkles:feat: função para compartilhar resultado do imc

--- src/components/Form/ResultImc/index.js
+++ src/components/Form/ResultImc/index.js
@@ -1,12 +1,25 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity, Share } from "react-native";
 import styles from "./styles";
 
 export default function ResultImc(props) {
+  const onShare = async () => {
+    const result = await Share.share({
+      message: `Meu imc hoje é: ${props.resultImc}`,
+    });
+  };
+
   return (
     <View style={styles.imcContext}>
       <Text style={styles.information}>{props.messageResultImc}</Text>
       <Text style={styles.resultImc}>{props.resultImc}</Text>
+      <View style={styles.boxSharedButton}>
+        {props.resultImc != null && (
+          <TouchableOpacity onPress={onShare} style={styles.sharedButton}>
+            <Text style={styles.textSharedButton}>Compartilhar</Text>
+          </TouchableOpacity>
+        )}
+      </View>
     </View>
   );
 }
--- src/components/Form/ResultImc/styles.js
+++ src/components/Form/ResultImc/styles.js
@@ -3,20 +3,35 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
   imcContext: {
     marginTop: 15,
-    paddingTop: 60,
+    paddingTop: 10,
     borderRadius: 50,
     alignItems: "center",
     width: "100%",
   },
-  resultImc:{
+  resultImc: {
     fontSize: 48,
     fontWeight: "bold",
     color: "#ff0043",
   },
-  information:{
+  information: {
     fontSize: 18,
     fontWeight: "bold",
     color: "#ff0043",
+  },
+  boxSharedButton: {
+    width: "100%",
+    alignItems: "center",
+    marginTop: 10,
+  },
+  sharedButton: {
+    backgroundColor: "#1877f2",
+    borderRadius: 50,
+    padding: 10,
+  },
+  textSharedButton: {
+    color: "#ffffff",
+    fontWeight: "bold",
+    paddingHorizontal: 30,
   },
 });
 
