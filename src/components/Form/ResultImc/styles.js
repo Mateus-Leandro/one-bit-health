87c68e7e24390439fff8a35aca71dce9ba9859f6
mateus.chagas <mateus.chagas@avancoinfo.com.br>
2025-04-15 06:26:14 -0300

    :lipstick:feat: estilização do componente do resultado do imc

---
+++ src/components/Form/ResultImc/styles.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  imcContext: {
+    marginTop: 15,
+    paddingTop: 60,
+    borderRadius: 50,
+    alignItems: "center",
+    width: "100%",
+  },
+  resultImc:{
+    fontSize: 48,
+    fontWeight: "bold",
+    color: "#ff0043",
+  },
+  information:{
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#ff0043",
+  },
+});
+
+export default styles;
