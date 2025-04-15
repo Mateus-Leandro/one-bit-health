d979f1dc1869fdf258e9f41204f4964fddb3da03
mateus.chagas <mateus.chagas@avancoinfo.com.br>
2025-04-15 07:30:13 -0300

    :sparkles:feat: mensagem de campos obrigatórios e função de vibração

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,5 +1,11 @@
 import React, { useState } from "react";
-import { View, Text, TextInput, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Vibration,
+} from "react-native";
 import ResultImc from "./ResultImc";
 import styles from "./style";
 
@@ -9,29 +15,49 @@ export default function Form() {
   const [messageImc, setMessageImc] = useState("Preencha o peso e altura");
   const [imc, setImc] = useState(null);
   const [textButton, setTextButton] = useState("Calcular");
+  const [weightErrorMessage, setWeightErrorMessage] = useState(null);
+  const [heightErrorMessage, setHeightErrorMessage] = useState(null);
+  const errorMessage = "*campo obrigatório";
 
   function imcCalculator() {
     return setImc((weight / (height * height)).toFixed(2));
   }
 
   function validationImc() {
-    if (weight != null && height != null) {
-      imcCalculator();
-      setHeight(null);
-      setWeight(null);
-      setMessageImc("Seu imc é igual: ");
-      setTextButton("Calcular Novamente");
+    let valid = true;
+    setWeightErrorMessage(null);
+    setHeightErrorMessage(null);
+
+    if (weight == null) {
+      setWeightErrorMessage(errorMessage);
+      valid = false;
+    }
+
+    if (height == null) {
+      setHeightErrorMessage(errorMessage);
+      valid = false;
+    }
+
+    if (!valid) {
+      setImc(null);
+      setTextButton("Calcular");
+      setMessageImc("Preencha o peso e a altura");
+      Vibration.vibrate();
       return;
     }
-    setImc(null);
-    setTextButton("Calcular");
-    setMessageImc("preencha o peso e altura");
+
+    imcCalculator();
+    setHeight(null);
+    setWeight(null);
+    setMessageImc("Seu imc é igual: ");
+    setTextButton("Calcular Novamente");
   }
 
   return (
     <View style={styles.formContext}>
       <View style={styles.form}>
         <Text style={styles.formLabel}>Altura</Text>
+        <Text style={styles.errorMessage}>{heightErrorMessage}</Text>
         <TextInput
           style={styles.input}
           onChangeText={setHeight}
@@ -40,6 +66,7 @@ export default function Form() {
           keyboardType="numeric"
         />
         <Text style={styles.formLabel}>Peso</Text>
+        <Text style={styles.errorMessage}>{weightErrorMessage}</Text>
         <TextInput
           style={styles.input}
           value={weight}
